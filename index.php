eb5c11e1b2e862cfc24d0f14e469826ebb1e7402
tehmissingk <karipap87@yahoo.com>
2015-10-31 10:25:39 +0800

    modul primary
    bug fixing

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 hello world
 hai nama saya roslan
 tapi roslan bukan nama sebenar saya
-nama sebenar saya din
+nama sebenar saya
+din saje
 tapi din pun bukan nama sebenar
 nama sebenar saya terpaksa dirahsiakan
